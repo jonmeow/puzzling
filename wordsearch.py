adedd9f7c412314ed8a2c78ce5abf114eaad5386
ThornInJonsSide <fake-non-colliging@email.com>
2020-10-24 21:17:08 -0700

    Wordsearch.py - Add option to output with spreadsheet coords, including an offset.

--- wordsearch.py
+++ wordsearch.py
@@ -3,6 +3,7 @@
 #               [-d dict_file] 
 #               [-p] # print the input grid
 #               [-r] # use insane dict
+#               [-s first_row_number] # output coords sn spreadsheet format
 #               <grid_file> 
 """
 Searches the grid for all words of length min_string_length or longer.
@@ -37,11 +38,12 @@ def _LoadWordsFromDictFile(dict_file):
   return set(word.strip().upper() for word in open(dict_file).readlines())
 
 
-def _PrintWordsInGrid(words, grid, nrows, ncols, min_len):
+def _FindWordsInGrid(words, grid, nrows, ncols, min_len):
+  words_found = list()
 
-  def _PrintIfWord(direction, word):
+  def _SaveIfWord(direction, word):
     if word in words:
-      print('%s at %02i, %02i, %s' % (word, row, col, direction))
+      words_found.append((word, row, col, direction))
 
   def _Reverse(word):
     return word[::-1]
@@ -70,33 +72,34 @@ def _PrintWordsInGrid(words, grid, nrows, ncols, min_len):
 
         # This ordering is for the ease of human parsing, not efficiency
         if _CanGoRight():
-          _PrintIfWord("across", grid[row][col:col+strlen])
+          _SaveIfWord("R", grid[row][col:col+strlen])
 
         if _CanGoLeft():
-          _PrintIfWord("left", _Reverse(grid[row][col-strlen+1:col+1]))
+          _SaveIfWord("L", _Reverse(grid[row][col-strlen+1:col+1]))
 
         if _CanGoDown():
-          _PrintIfWord("down", 
-                       "".join([grid[row+i][col] for i in range(strlen)]))
+          _SaveIfWord("D", "".join([grid[row+i][col] for i in range(strlen)]))
 
           if _CanGoRight():
-            _PrintIfWord("down right",
+            _SaveIfWord("DR",
                          "".join([grid[row+i][col+i] for i in range(strlen)]))
 
           if _CanGoLeft():
-            _PrintIfWord("down left",
+            _SaveIfWord("DL",
                          "".join([grid[row+i][col-i] for i in range(strlen)]))
 
         if _CanGoUp():
-          _PrintIfWord("up", "".join([grid[row-i][col] for i in range(strlen)]))
+          _SaveIfWord("U", "".join([grid[row-i][col] for i in range(strlen)]))
 
           if _CanGoRight():
-            _PrintIfWord("up right",
+            _SaveIfWord("UR",
                          "".join([grid[row-i][col+i] for i in range(strlen)]))
 
           if _CanGoLeft():
-            _PrintIfWord("up left",
+            _SaveIfWord("UL",
                          "".join([grid[row-i][col-i] for i in range(strlen)]))
+
+  return words_found
 
           
 def _ParseCommandLineArguments(argv):
@@ -114,23 +117,53 @@ def _ParseCommandLineArguments(argv):
         help="The file containing the grid to search.")
 
     parser.add_argument("-l", "--min_length", type=int, default=4,
-        help="The minimum length of word to find. Default 4.")
+        help="The minimum length of word to find. Default 4.\n ")
+        # The trailing "\n " creates a separator in the help output
 
-    parser.add_argument("-d", "--dict_file", default=HUGE_DICT_FILE,
-        help="Dictionary to use.\nDefault: %s" % HUGE_DICT_FILE)
+    parser.add_argument("-d", "--dict_file", default=HUGE_DICT_FILE, 
+        metavar="file", help="Dictionary to use.\nDefault: %s" % HUGE_DICT_FILE)
+
+    parser.add_argument("-r", "--find_rare_words", action="store_true",
+        help="Find rarer words in the solution (bigger dictionary)."
+             "\nThis overrides --dict_file\n ")
 
     parser.add_argument("-p", "--print_grid", action="store_true",
         help="Print the grid to be searched.")
 
-    parser.add_argument("-r", "--allow_rare_words", action="store_true",
-        help="Allow rarer words in the solution (bigger dictionary)."
-             "\nThis overrides --dict_file")
+    parser.add_argument("-s", "--spreadsheet_numbering", nargs=2, metavar="s",
+        help="Print the found coords as row(#), col(letter)."
+             "\nstarting the row and letter given, e.g. -s 1 A.")
 
     args = parser.parse_args()
-    if args.allow_rare_words:
+    if args.find_rare_words:
       args.dict_file = RARE_DICT_FILE
 
+    if not args.spreadsheet_numbering:
+      args.spreadsheet_numbering = [0,0]
+
     return args
+
+def  _PrintWordsFound(words_found, top_left_row_number, top_left_col_letter):
+  """If top_left entries are specified, use spreadsheet coords, e.g. 1A.
+     If the letter gets past Z, start over at AA.
+     If the letter goes past AZ, print crap.
+  """
+  if not (top_left_row_number or top_left_col_letter):
+    for word, row, col, dir in words_found:
+      print('%s at %02i, %02i, %s' % (word, row, col, dir))
+    return
+
+  def _ModRow(r):
+    return r + int(top_left_row_number)
+
+  def _ModCol(c):
+    new_ord = ord(top_left_col_letter) + c
+    if new_ord <= ord('Z'):
+      return chr(new_ord)
+    return "A" + chr(new_ord-26)
+
+  for word, row, col, dir in words_found:
+    print('%s at %02i, %2s, %s' % (word, _ModRow(row), _ModCol(col), dir))
 
 
 def Main():
@@ -139,9 +172,10 @@ def Main():
   dict_file = args.dict_file
   grid_file = args.grid_file[0]
   print_grid_file = args.print_grid
+  top_left_row_number = args.spreadsheet_numbering[0]
+  top_left_col_letter = args.spreadsheet_numbering[1]
 
   grid = _LoadGridFromFile(grid_file)
-
   if print_grid_file:
     PrettyPrinter(indent=2).pprint(grid)
 
@@ -154,7 +188,8 @@ def Main():
 
   words = _LoadWordsFromDictFile(dict_file)
 
-  _PrintWordsInGrid(words, grid, nrows, ncols, min_len)
+  words_found = _FindWordsInGrid(words, grid, nrows, ncols, min_len)
+  _PrintWordsFound(words_found, top_left_row_number, top_left_col_letter)
 
 
 if __name__ == '__main__':
